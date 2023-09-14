844693720634706e1025c12b50d4f91c3da93509
Sayli-1015 <sayli.t@sankeysolutions.com>
2023-09-14 15:15:47 +0530

    fix: Navbar file extension

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Navbar from "./components/Navbar/Navbar.js";
+import Navbar from "./components/Navbar/Navbar.jsx";
 
 function App() {
   const [count, setCount] = useState(0)
