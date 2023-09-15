923ffb6841f7faf19c5428b5002821211116ff8b
Sayli-1015 <sayli.t@sankeysolutions.com>
2023-09-15 10:47:41 +0530

    feat: added routes to home page and barcode page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import './App.css'
+import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Navbar from "./components/Header/Navbar.jsx";
 import Footer from "./components/Footer/Footer.jsx";
+import Home from "./Pages/Home.jsx";
+import BarcodePatternMaster from "./Pages/BarcodePatternMaster.jsx";
 
 function App() {
   return (
-    <>
+      <>
+        <>
         <div className="container">
             <Navbar/>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/barcode' element={<BarcodePatternMaster />} />
+            </Routes>
             <Footer/>
         </div>
-    </>
+        </>
+      </>
   )
 }
 
---
+++ src/Pages/BarcodePatternMaster.jsx
@@ -0,0 +1,9 @@
+const BarcodePatternMaster = () => {
+    return(
+        <>
+            <h1>Barcode Pattern Master</h1>
+        </>
+    );
+}
+
+export default BarcodePatternMaster;
